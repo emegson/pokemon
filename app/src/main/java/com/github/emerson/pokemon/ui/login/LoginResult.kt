7b4de0bb57e191309bf6145737c85c2490287e67
Emerson Almeida Matos <emegson@live.com>
2020-03-05 23:15:28 -0300

    initial commit

---
+++ app/src/main/java/com/github/emerson/pokemon/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.github.emerson.pokemon.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
